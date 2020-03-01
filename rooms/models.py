b446c24fdba1ee11cdba9fd8a646671d2903ab4c
hiryu826 <45277948+hiryu826@users.noreply.github.com>
2020-03-01 16:28:58 +0900

    Users, Rooms App
    Users, Rooms App

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as user_models
+
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    class Meta:
+        verbose_name = "Room Type"
+        # ordering = ["name"]
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    # room이 지워질 시 Photo도 함께 지워지도록 설정
+    # Python은 파일을 상하수직으로 읽기때문에 Room이 정의되어있지 않으므로 밑으로 옮겨주어야 한다.
+    # 또는 String으로 ForeignKey를 생설할 수 있다. (선택 된 모든 것들이 string이 된다. / User를 Importing 할 필요가 없어진다.)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
 
 # Create your models here.
+class Room(core_models.TimeStampedModel):
+
+    """ Room Model Definition """
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    guests = models.IntegerField()
+    bed = models.IntegerField()
+    bedroom = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
+
+
+def __str__(self):
+    return self.name
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+# class CustomUserAdmin(admin.ModelAdmin):
+# username, gender, language, currency, superhost tables 생성
+# list_display = ("username", "email", "gender", "language", "currency", "superhost")
+# list_filter = ("language","currency","superhost")
+
+
+@admin.register(models.User)  # decorator 방식
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
