36fec19426830e11577f84f34af0dc3bd762397c
hiryu826 <45277948+hiryu826@users.noreply.github.com>
2020-03-17 21:03:16 +0900

    (2020.03.17) Search View
    Search View

--- rooms/views.py
+++ rooms/views.py
@@ -6,6 +6,8 @@
 # from django.urls import reverse
 # from django.http import Http404
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -23,7 +25,70 @@ class HomeView(ListView):
 
 class RoomDetail(DetailView):
 
+    """ RoomDetail Definition """
+
     model = models.Room
+
+
+# search view(function based)
+def search(request):
+    print(request.GET)
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    s_amenities = request.GET.getlist("amenities", 0)
+    s_facilities = request.GET.getlist("facilities", 0)
+
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    # 필터 조건 작성
+    filter_args = {}
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        # roomtype foreignkey를 이용한 filter
+        # filter_args["room_type__pk__exact"] = room_type
+        filter_args["room_type__pk"] = room_type
+
+    # 필터 생성
+    rooms = models.Room.objects.filter(**filter_args)
+
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms})
 
 
 # -----------------------------------
@@ -90,4 +155,3 @@ class RoomDetail(DetailView):
 # now = datetime.now()
 # hungry = True
 # return render(request, "all_rooms.html")  # django에게 all_rooms.html 컴파일을 위한 명령
-
