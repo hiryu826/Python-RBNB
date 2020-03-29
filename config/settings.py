f73ffb3fb4c478095ede9937fd49e492e499d98b
username <45277948+hiryu826@users.noreply.github.com>
2020-03-30 02:45:37 +0900

    (2020.03.30) AWS S3
    (2020.03.30) AWS S3

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_countries", "django_seed"]
+THIRD_PARTY_APPS = ["django_countries", "django_seed", "storages"]
 
 # User app 설치
 PROJECT_APPS = [
@@ -174,14 +174,12 @@ if not DEBUG:
     STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = "airbnb-clone-nomadcoders"
+    AWS_STORAGE_BUCKET_NAME = "RBNB-MrRyu"
     AWS_AUTO_CREATE_BUCKET = True
     AWS_BUCKET_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-
-    # Sentry
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
